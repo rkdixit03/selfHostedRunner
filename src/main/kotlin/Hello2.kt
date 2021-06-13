2aeaae24fdf499651329aa856a488b77c6ab04bb
Rajesh Kumar <rkdixit3@gmail.com>
2021-06-14 03:14:08 +0530

    Create Hello2.kt

---
+++ src/main/kotlin/Hello2.kt
@@ -0,0 +1,14 @@
+package hello
+
+data class Person2(val name: String,val age: Int? = null)
+
+fun main2(args: Array<String>) {
+    val persons = listOf(Person2("Alice"),
+                         Person2("Bob", age = 29))
+
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("The oldest is: $oldest")
+}
+
+// The oldest is: Person(name=Bob, age=29)
+
